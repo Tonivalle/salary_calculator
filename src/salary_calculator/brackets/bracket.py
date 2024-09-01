f307a12a4228d7781b96c7bdd5bf388692ee6580
Tonivalle <tonivalle15@gmail.com>
2024-09-01 20:11:01 +0200

    Bracket tests expanded

--- src/salary_calculator/brackets/bracket.py
+++ src/salary_calculator/brackets/bracket.py
@@ -4,9 +4,23 @@ from typing import Any
 
 @dataclass
 class TaxBracket:
+    """
+    Represents a tax bracket.
+
+    The salary between `low` and `high` gets taxed `percentage` amount.
+    """
     low: int
+    """
+    Lowest amount included in this bracket (inclusive).
+    """
     high: int
+    """
+    Highers amount in the bracket (exclusive).
+    """
     percentage: float
+    """
+    Percentage of the salary in that bracket that is taken as taxes.
+    """
 
     def __contains__(self, item: Any) -> bool:
-        return self.low < item <= self.high
+        return self.low <= item < self.high
